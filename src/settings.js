6fe0ef249ef9ff58634b33572be9e835eba5a85d
Don <kibetdonald97@gmail.com>
2022-05-16 16:59:45 +0300

    Solved merge conflict

--- src/settings.js
+++ src/settings.js
@@ -6,6 +6,7 @@ import {
 } from "agora-rtc-react";
 
 const appId = "902b2e809ca54994ba1af501720d3c0f";
+
 const token =
   "006d35d286913e24d7688fc2f43dbce9da1IACSVB4eNwWAHI8XyNL483LfWAlx1iW3TgG5vf9hWbcNQGTNKL8AAAAAEAASVDxDi0SCYgEAAQCKRIJi";
 
@@ -13,6 +14,3 @@ export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
 export const channelName = "main";
-
-
-
